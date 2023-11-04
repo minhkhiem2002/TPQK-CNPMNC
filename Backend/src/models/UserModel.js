578a2f76b29ee577b2a69c4f718d73b1494f3f62
HoangPhiem <tuhoangphiem10@gmail.com>
2023-11-04 13:23:02 +0700

    fix jwt service

--- Backend/src/models/UserModel.js
+++ Backend/src/models/UserModel.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-            maxLength: 50
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            maxLength: 50
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            maxLength: 50
-        },
-        role: {
-            type: String,
-            enum: ['manager', 'user', 'finance'],
-            default: 'user'
-        },
-        department: {
-            type: String,
-            required: true
-        }
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    role: {
+      type: String,
+      enum: ["manager", "user", "finance"],
+      default: "user",
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 const User = mongoose.model("User", userSchema);
 module.exports = User;
